4b0db12ec6f1fee974a8d1c28c1b5ba7c906bb70
Roman Vashchenko <roman1991dp@gmail.com>
2024-04-12 16:43:01 +0300

    update

---
+++ js/menu-container.js
@@ -0,0 +1,28 @@
+const refs = {
+  menuContainer: document.querySelector(".menu-container "),
+  btnMenu: document.querySelector(".navigation__menu"),
+  list: document.querySelector(".menu-container__list"),
+};
+
+refs.btnMenu.addEventListener("click", onMenuClick);
+
+function onMenuClick() {
+  refs.menuContainer.classList.toggle("js-open");
+  if (refs.menuContainer.classList.contains("js-open")) {
+    document.addEventListener("click", onCloseMenu);
+  }
+  if (!refs.menuContainer.classList.contains("js-open")) {
+    document.removeEventListener("click", onCloseMenu);
+  }
+}
+
+function onCloseMenu(e) {
+  if (e.target === refs.list) {
+    return;
+  }
+  if (refs.btnMenu !== e.target) {
+    refs.menuContainer.classList.remove("js-open");
+    refs.menuContainer.classList.add("js-close");
+    document.removeEventListener("click", onCloseMenu);
+  }
+}
